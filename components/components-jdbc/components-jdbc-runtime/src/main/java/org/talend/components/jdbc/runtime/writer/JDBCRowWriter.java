8e4e71a624b7bc9b6dd540bb7e32743566e4e960
wang wei <wwang@talend.com>
2017-09-11 15:38:05 +0800

    more test

--- components/components-jdbc/components-jdbc-runtime/src/main/java/org/talend/components/jdbc/runtime/writer/JDBCRowWriter.java
+++ components/components-jdbc/components-jdbc-runtime/src/main/java/org/talend/components/jdbc/runtime/writer/JDBCRowWriter.java
@@ -35,7 +35,6 @@ import org.talend.components.api.exception.ComponentException;
 import org.talend.components.api.properties.ComponentProperties;
 import org.talend.components.common.avro.JDBCAvroRegistry;
 import org.talend.components.jdbc.CommonUtils;
-import org.talend.components.jdbc.ComponentConstants;
 import org.talend.components.jdbc.JDBCTemplate;
 import org.talend.components.jdbc.RuntimeSettingProvider;
 import org.talend.components.jdbc.runtime.JDBCRowSink;
@@ -82,11 +81,11 @@ public class JDBCRowWriter implements WriterWithFeedback<Result, IndexedRecord, 
 
     private ResultSet resultSet;
 
-    private int insertCount;
-
-    private int updateCount;
-
-    private int deleteCount;
+    // private int insertCount;
+    //
+    // private int updateCount;
+    //
+    // private int deleteCount;
 
     private boolean usePreparedStatement;
 
@@ -342,6 +341,7 @@ public class JDBCRowWriter implements WriterWithFeedback<Result, IndexedRecord, 
     }
 
     private void constructResult() {
+        /*
         if (runtime != null) {
             runtime.setComponentData(runtime.getCurrentComponentId(),
                     CommonUtils.getStudioNameFromProperty(ComponentConstants.RETURN_DELETE_RECORD_COUNT), deleteCount);
@@ -350,6 +350,7 @@ public class JDBCRowWriter implements WriterWithFeedback<Result, IndexedRecord, 
             runtime.setComponentData(runtime.getCurrentComponentId(),
                     CommonUtils.getStudioNameFromProperty(ComponentConstants.RETURN_UPDATE_RECORD_COUNT), updateCount);
         }
+        */
 
         result.successCount = successCount;
         result.rejectCount = rejectCount;
