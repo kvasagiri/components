9058909950da5c283dd90acbd3b1ce743126c8f3
wang wei <wwang@talend.com>
2017-09-22 15:16:28 +0800

    fix some bug in the tujs

--- components/components-jdbc/components-jdbc-runtime/src/main/java/org/talend/components/jdbc/runtime/writer/JDBCOutputWriter.java
+++ components/components-jdbc/components-jdbc-runtime/src/main/java/org/talend/components/jdbc/runtime/writer/JDBCOutputWriter.java
@@ -173,6 +173,8 @@ abstract public class JDBCOutputWriter implements WriterWithFeedback<Result, Ind
             }
         }
 
+        // the var is equals with the old "nb_line" var, but in the old jdbc components and old tujs, "nb_line" means the success
+        // rows, so strange, TODO move it to the right location to make the tujs work
         result.totalCount++;
         cleanWrites();
     }
@@ -272,8 +274,10 @@ abstract public class JDBCOutputWriter implements WriterWithFeedback<Result, Ind
             return result;
         }
 
+        commitCount++;
+        
         if (commitCount < commitEvery) {
-            commitCount++;
+            
         } else {
             commitCount = 0;
 
@@ -295,8 +299,10 @@ abstract public class JDBCOutputWriter implements WriterWithFeedback<Result, Ind
         if (useBatch) {
             statement.addBatch();
 
+            batchCount++;
+            
             if (batchCount < batchSize) {
-                batchCount++;
+                
             } else {
                 batchCount = 0;
                 count = executeBatchAndGetCount(statement);
@@ -360,12 +366,14 @@ abstract public class JDBCOutputWriter implements WriterWithFeedback<Result, Ind
                     CommonUtils.getStudioNameFromProperty(ComponentConstants.RETURN_INSERT_RECORD_COUNT), insertCount);
             runtime.setComponentData(runtime.getCurrentComponentId(),
                     CommonUtils.getStudioNameFromProperty(ComponentConstants.RETURN_UPDATE_RECORD_COUNT), updateCount);
+            runtime.setComponentData(runtime.getCurrentComponentId(),
+                    CommonUtils.getStudioNameFromProperty(ComponentConstants.RETURN_REJECT_RECORD_COUNT), rejectCount);
         }
 
         result.successCount = successCount;
         result.rejectCount = rejectCount;
     }
-    
+
     protected int executeBatchAtLast() {
         if (useBatch && batchCount > 0) {
             try {
@@ -379,7 +387,7 @@ abstract public class JDBCOutputWriter implements WriterWithFeedback<Result, Ind
                 }
             }
         }
-        
+
         return 0;
     }
 
